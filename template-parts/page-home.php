0ce4afae198985f1e8ad82bf85969919c311736d
jakub <jakub.webdesign@gmail.com>
2018-03-14 07:52:19 +0100

    add content to home page

--- template-parts/page-home.php
+++ template-parts/page-home.php
@@ -5,4 +5,258 @@ Template Name: Home Page
 
 get_header();?>
 
+  <!--===========main-->
+  <section id="main" data-type="background" data-speed="5">
+    <article>
+      <div class="container clearfix">
+        <div class="row">
+          <div class="col-sm-5">
+            <img src="resources/img/logo-bootstrap.png" alt="Bootstrap Logo" class="logo">
+          </div>
+          <div class="col-sm-7 main-text">
+            <h1>Jakub Wordpress Bootstrap Theme</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div id="price-timeline">
+              <div class="price active">
+                <h4>Pre launch price <small>Check It out</small></h4>
+                <span>$149</span>
+              </div>
+              <div class="price">
+                <h4>Launch Price <small>Check It out</small></h4>
+                <span>$200</span>
+              </div>
+              <div class="price">
+                <h4>Final Price <small>Check It out</small></h4>
+                <span>300$</span>
+              </div>
+            </div>
+            <p>
+              <a href="#" class="btn btn-lg btn-danger" role="button">Subscribe</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+  </section>
+  <!--===========OPT-->
+  <section id="options">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <p class="lead">
+            <strong>Subscribe to our mail list </strong>It is worth it ! and costs nothing.
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <button class="btn btn-lg btn-success btn-block" id="btn-sign" data-toggle="modal" data-target="#myModal">Click here to sucbscribe</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Income-->
+  <section id="boost-income">
+    <div class="container">
+      <div class="section-header">
+        <img src="resources/img/icon-boost.png" alt="Boots">
+        <h2>How to boos income</h2>
+      </div>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>Make money on this theme</h4>
+          <p class="main_boost">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="col-sm-6">
+          <h4>Full time income</h4>
+          <p class="main_boost">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Benefits-->
+  <section id="benefit">
+    <div class="container">
+      <div class="section-header">
+        <img src="resources/img/icon-pad.png" alt="Pad">
+        <h2>Who is this website for ?</h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 offset-sm-2">
+          <h3>Wordpress Designers &amp; Developers</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Web Designers &amp; Developers</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Students</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Graphic Designers</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Feature-->
+  <section id="feature">
+    <div class="container">
+      <div class="section-header">
+        <img src="resources/img/icon-rocket.png" alt="Rocket">
+        <h2>Course Features</h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-2">
+          <i class="ci ci-computer"></i>
+          <h4>life time access to course</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-watch"></i>
+          <h4>Avaiable at any time</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-calendar"></i>
+          <h4>30 days of support</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-community"></i>
+          <h4>Join us to become a happy user !</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-device"></i>
+          <h4>Compatible at any device</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-instructor"></i>
+          <h4>Made by developer </h4>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Project-->
+  <section id="project-section">
+    <div class="container">
+      <h2>Summary of Wordpress theme</h2>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <strong>laboris nisi ut aliquip</strong> ex ea commodo consequat.</p>
+      <div class="row">
+        <div class="col-sm-4">
+          <img src="resources/img/icon-design.png" alt="Design">
+          <h3>Super Modern and Awesome Design</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="resources/img/icon-code.png" alt="Code">
+          <h3>Great quality and clean code</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="resources/img/icon-design.png" alt="Design">
+          <h3>Super Modern and Awesome Design</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Video-->
+  <section id="video-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 offset-sm-2">
+          <h2>Watch our promo video</h2>
+          <iframe width="100%" height="415" src="https://www.youtube.com/embed/quiUytHXutM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Instructor-->
+  <section id="instructor">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-6">
+          <div class="row">
+            <div class="col-lg-8">
+              <h2>It is me <small>Jakub Biesek</small> </h2>
+            </div>
+            <div class="col-lg-4" id="social-circles">
+              <a href="https://github.com/JK-Biesek/" target="_blank" class="badge social github"><i class="fa fa-github"></i></a>
+              <a href="#" target="_blank" class="badge social facebook"><i class="fa fa-facebook"></i></a>
+              <a href="https://www.linkedin.com/in/jakub-biesek-b2646511a/" target="_blank" class="badge social linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <hr>
+          <h3>Summary of my work</h3>
+          <div class="row">
+              <div class="col-xs-4">
+                <div class="number">
+                  <div class="number-circle">
+                    9 Github <span>repositories</span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-4">
+                <div class="number">
+                  <div class="number-circle">
+                    1 year of<span>experience</span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-4">
+                <div class="number">
+                  <div class="number-circle">
+                    4 Years done in <span>Computing</span>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--===========Testimonials-->
+  <section id="opinions">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 offset-sm-2">
+          <h2 id="opinion">What People think about Jakub</h2>
+          <div class="row testimonial">
+            <div class="col-md-4">
+              <img src="resources/img/aj.png" alt="Opinion">
+            </div>
+            <div class="col-md-8">
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <cite>&mdash; Jakub, completed VueJS2 course</cite>
+              </blockquote>
+            </div>
+          </div>
+          <div class="row testimonial">
+            <div class="col-md-4">
+              <img src="resources/img/ben.png" alt="Opinion">
+            </div>
+            <div class="col-md-8">
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <cite>&mdash; Jakub, completed VueJS2 course</cite>
+              </blockquote>
+            </div>
+          </div>
+          <div class="row testimonial">
+            <div class="col-md-4">
+              <img src="resources/img/ernest.png" alt="Opinion">
+            </div>
+            <div class="col-md-8">
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <cite>&mdash; Jakub, completed VueJS2 course</cite>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 	<?php get_footer();?>
