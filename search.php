ef6bcc19960246e97783f74ba5c8d61e7881e127
jakub <jakub.webdesign@gmail.com>
2018-03-19 16:15:56 +0100

    fix up search page code and style some css

--- search.php
+++ search.php
@@ -9,21 +9,20 @@
 
 get_header();
 ?>
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="4">
+	<h1 class="page-title">
+		<?php
+		/* translators: %s: search query. */
+		printf( esc_html__( 'Search Results for: %s', 'simplytheme4wordpress' ), '<span>' . get_search_query() . '</span>' );
+		?>
+	</h1>
+</section>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div class="container">
+    <div class="row" id="primary">
+		<main id="content" class="col-sm-8">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'simplytheme4wordpress' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
@@ -37,19 +36,19 @@ get_header();
 				get_template_part( 'template-parts/content', 'search' );
 
 			endwhile;
-
 			the_posts_navigation();
 
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main>
+	<div class="col-sm-4">
+		<?php get_sidebar(); ?>
+	</div>
+	</div>
+</div>
 
 <?php
-get_sidebar();
 get_footer();
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,23 +11,38 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		endif;
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			simplytheme4wordpress_posted_on();
-			simplytheme4wordpress_posted_by();
+		if ( 'post' === get_post_type() ) :
 			?>
-		</div><!-- .entry-meta -->
+			<div class="post-details">
+              <span class="fa fa-user"></span><?php the_author(); ?>
+              <span class="fa fa-calendar"></span><?php the_date(); ?>
+              <span class="fa fa-folder"></span><?php the_category(' , '); ?>
+              <span class="fa fa-tag"></span><?php the_tags(' ',',',' '); ?>
+              <div class="post-comments-badge">
+                <a href="<?php comments_link(); ?>"><span class="fa fa-comments"></span><?php comments_number(0,1,'%'); ?></a>
+              </div>
+									<?php edit_post_link('Edit',' <i class="fa fa-pencil"></i>',''); ?>
+            </div>
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php simplytheme4wordpress_post_thumbnail(); ?>
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	<?php
+	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "medium_large" );
+		if(has_post_thumbnail() ) : echo '
+			<div class="post-images">
+				<img src="'.$thumbnail[0].'" alt="Blog image" >
+			</div>';
+		endif; ?>
+	<div class="post-excerpt">
+		<p><?php the_excerpt(); ?></p>
+	</div>
 
 	<footer class="entry-footer">
 		<?php simplytheme4wordpress_entry_footer(); ?>
