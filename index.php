7f199648f596d315a3ae79d06fc355a33f24bb93
jakub <jakub.webdesign@gmail.com>
2018-03-19 10:30:54 +0100

    adding blog code

--- index.php
+++ index.php
@@ -14,42 +14,51 @@
 
 get_header();
 ?>
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="4">
+	 <h1 class="page-title">Blog</h1>
+ </section>
+ <div class="container">
+	 <div class="row" id="primary">
+		 <main id="content" class="col-sm-8" role="main">
+			 <?php
+	 		if ( have_posts() ) :
 
+	 			if ( is_home() && ! is_front_page() ) :
+	 				?>
+	 				<header>
+	 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+	 				</header>
+	 				<?php
+	 			endif;
+
+	 			/* Start the Loop */
+	 			while ( have_posts() ) :
+	 				the_post();
+
+	 				/*
+	 				 * Include the Post-Type-specific template for the content.
+	 				 * If you want to override this in a child theme, then include a file
+	 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+	 				 */
+	 				get_template_part( 'template-parts/content', get_post_type() );
+
+	 			endwhile;
+
+	 			the_posts_navigation();
+
+	 		else :
+
+	 			get_template_part( 'template-parts/content', 'none' );
+
+	 		endif;
+	 		?>
+		 </main>
+	 </div>
+ </div> <!--Container-->
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
