aea70017874bce29965349c6e4a7f47219704588
jakub <jakub.webdesign@gmail.com>
2018-03-24 11:57:57 +0100

    add contact page

---
+++ page-contact.php
@@ -0,0 +1,13 @@
+<?php
+/*Template Name: Contact Template*/
+get_header();
+ ?>
+ <div class="container">
+    <div class="row" id="primary">
+      <div class="col-sm-12" id="content">
+        
+      </div>
+    </div>
+
+  </div>
+<?php get_footer(); ?>
