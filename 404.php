f14f7e76039c3807535b9f0dfebef885ca399ef5
jakub <jakub.webdesign@gmail.com>
2018-03-25 00:06:12 +0100

    change error php code

--- 404.php
+++ 404.php
@@ -9,51 +9,23 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'simplytheme4wordpress' ); ?></h1>
-				</header><!-- .page-header -->
-
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="4">
+	<h1 class="page-title">Uuuupss Page not Found :( </h1>
+</section>
+	<div class="container">
+		<div class="col-sm-8" id="primary">
+			<div class="error-404 not-found">
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'simplytheme4wordpress' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'simplytheme4wordpress' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$simplytheme4wordpress_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'simplytheme4wordpress' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$simplytheme4wordpress_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
+					<h2>I will get you out of here !</h2>
+					<h3>Maybe you were looking for some of the information</h3>
+					<div class="">
+						<?php while ($loop->have_posts() ) : $loop->the_post();
+						
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div><!-- #primary -->
 
 <?php
